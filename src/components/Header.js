28f184241538ed5cf2d21f8c76b2a2e28860f39a
ramenNoodles1998 <48036775+ramenNoodles1998@users.noreply.github.com>
2019-11-05 12:34:34 -0700

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,26 @@ function Header(){
         <div>
             <div className="row Header ">
                 
-                <h1 style={{color: "#141824", fontSize: "30px", paddingTop: "30px"}} className="text-left col-sm">
+                <h1 style={{color: "#141824", fontSize: "30px", paddingTop: "30px"}} className="text-center col-10">
                     &nbsp;&nbsp;A PORTFOLIO By
                 </h1>           
             </div>
             <div className="row Header">
                 
-                <h1 style={{color: "#141824", fontSize: "100px"}} className="text-left col-sm">
+                <h1 style={{color: "#141824", fontSize: "100px"}} className="text-center col-12">
                     ROMAN
                 </h1>           
             </div>
             <div className="row Header ">
                 
-                <h1 style={{fontSize: "73px"}} className ="text-left col-sm">
+                <h1 style={{fontSize: "73px"}} className ="text-center col-12">
                     MEREDITH
                 </h1>
             </div>
             
             <div className="row">
-                <div style={{paddingTop: "30px"}}className="col-sm-12 text-center">
-                    Software Developer
+                <div style={{padding: "30px", background: "#c4d3ff", color: "whitesmoke", fontFamily: "Overpass"}}className="col-sm-12 text-center">
+                    A Software Developer
                 </div>
 
             </div>
