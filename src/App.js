165a2071d2a593bc74674f1a2f661734c348f24b
ramenNoodles1998 <48036775+ramenNoodles1998@users.noreply.github.com>
2019-11-06 00:00:22 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import ProjectsTitle from './components/ProjectsTitle'
+import Projects from './components/Projects'
+
 
 function App() {
   return (
     <div className="container-fluid">
       <Header/>
+      <Body/>
+      <ProjectsTitle/>
+      <Projects 
+        name="WHAT'S THE COOLDOWN?"
+        num="1"
+        hrefP=""
+        hrefC=""
+      />
+      <Projects 
+        name="WHAT'S THE COOLDOWN?"
+        num="1"
+        hrefP=""
+        hrefC=""
+      />
+      <Projects 
+        name="WHAT'S THE COOLDOWN?"
+        num="1"
+        hrefP=""
+        hrefC=""
+      />
+      <Footer/>
     </div>
     
   );
