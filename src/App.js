49113df37b9b426528a5b34d2169be7bcabfed08
ramenNoodles1998 <48036775+ramenNoodles1998@users.noreply.github.com>
2019-11-07 21:39:29 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import './App.css'
+<<<<<<< HEAD
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Link, 
+  Redirect} from "react-router-dom"
+import MainPage from './pages'
+import lolApi from './pages/lolApi'
+import NotFoundPage from './pages/404'
+=======
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
 import ProjectsTitle from './components/ProjectsTitle'
 import Projects from './components/Projects'
+>>>>>>> 165a2071d2a593bc74674f1a2f661734c348f24b
 
 
 function App() {
   return (
+<<<<<<< HEAD
+  <Router>
+    <Switch>
+      <Route exact path="/" component={MainPage}/>
+      <Route path="/lolApi" component={lolApi}/>
+      <Route component={NotFoundPage}/>
+    </Switch> 
+  </Router>
+  )
+=======
     <div className="container-fluid">
       <Header/>
       <Body/>
@@ -35,6 +57,7 @@ function App() {
     </div>
     
   );
+>>>>>>> 165a2071d2a593bc74674f1a2f661734c348f24b
 }
 
 export default App;
